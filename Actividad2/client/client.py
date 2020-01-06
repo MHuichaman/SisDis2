7f501d6d43123978481affc77d2ebc38d76fe25c
Miguel <miguel.h@poliglota.cl>
2020-01-05 22:20:23 -0300

    testing it

--- Actividad2/client/client.py
+++ Actividad2/client/client.py
@@ -3,34 +3,21 @@ from datetime import datetime
 import uuid
 import logging
 import json
-
+import threading
+import time
 
 class Client():
 
   def __init__(self):
     print("Iniciando")
-
+    time.sleep(5)
 
     self.is_on = False
     self.user = ""
     self.user_id = str(uuid.uuid4())
 
-    self.connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmw', heartbeat=0))
+    self.connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq', heartbeat=0))
     self.channel = self.connection.channel()
-
-  def Connect(self):
-
-    while not self.is_on:
-      user = input("Ingrese usuario: ")
-      if user.strip():
-        self.log(user, "log_in")
-
-    self.user = user
-
-  def Leave(self):
-    self.log(user, "log_out")
-    self.connection.close()
-
 
   def get_msgs(self):
     connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq', heartbeat=0))
@@ -38,20 +25,100 @@ class Client():
     channel = connection.channel()
     channel.exchange_declare(exchange="broadcast",exchange_type="fanout")
 
+    exclusive_queue = channel.queue_declare(queue='', exclusive= True)
+    queue = exclusive_queue.method.queue
+
+    channel.queue_bind(exchange='broadcast', queue= queue)
+
+    def callback_get_msgs(ch, method, properties, body):
+
+      message = json.loads(body.decode("utf-8"))
+
+      user = message["user"]
+      content = message["user"]
+      time_now = message["time_now"]
+      date = (datetime.fromtimestamp(time_now)).strftime("%d/%m/%Y - %H:%M:%S")
+
+      print("[ " + date + " - " + user + " ]:" + content + " ")
+      
+    channel.basic_consume(
+      queue=queue,
+      on_message_callback=callback_get_msgs,
+      auto_ack=True,
+      consumer_tag=self.user
+    )
+
+    channel.start_consuming()
+
+  def get_server_msgs(self):
+    connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq', heartbeat=0))
+
+    channel = connection.channel()
+
+    exclusive_queue = channel.queue_declare(queue=self.user_id, exclusive= True)
+    queue = exclusive_queue.method.queue
+
+    channel.queue_bind(exchange='user_channel', queue= queue)
+
+    def callback_get_server_msgs(ch, method, properties, body):
+
+      response_from_server = json.loads(body.decode("utf-8"))
+
+      user = response_from_server["user"]
+      content = response_from_server["content"]
+      kind = response_from_server["kind"]
+      response = response_from_server["response"]
+
+      if kind == "log_in":            
+        if response == "Yay":
+            self.is_logged = True
+            print("Inicio correcto!!")
+
+        else:
+            print("Nombre de usuario ya registrado\n")
+        self.checked.set()
+
+      elif kind == "user_messages":
+          print("\n--------------")
+          print("Mensajes enviados: \n-----\n")
+
+          for sent_message in response:
+            json_message = json.loads(sent_message)
+            user = json_message["user"]
+            content =json_message["message"]
+            date = datetime.fromtimestamp(json_message["time_now"])
+            date_time = date.strftime("%m/%d/%Y, %H:%M:%S")
+
+            print("[ + " + date_time + " - " + user + "]: " + content + " ")
+
+      # Si es un mensaje con la lista de usuarios conectados al chat.
+      elif kind == "users_list":
+          print("\n-----------------------------")
+          print("Lista de usuarios: \n-------\n")
+          for user in response:
+              print(user)
+      else:
+          print("Rip")
+      
+    channel.basic_consume(
+      queue=queue,
+      on_message_callback=callback_get_server_msgs,
+      auto_ack=True
+    )
+
+    channel.start_consuming()
     
-  def send_mesage(self, kind, content):
+  def send_message(self, content, kind):
 
     time_now = datetime.timestamp(datetime.now())
     
     if content.strip():
-
       if kind == "log_in":
-
         message = {
           'message_id' : str(uuid.uuid4()),
           'kind': "log_in", #to identify source on server side
           'user': content,
-          'user_id': self.user_id,
+          'user_id': str(self.user_id),
           'time_stamp': time_now
         }
       elif kind == "log_out":
@@ -60,19 +127,61 @@ class Client():
           'message_id' : str(uuid.uuid4()),
           'kind': "log_out", #to identify source on server side
           'user': content,
-          'user_id': self.user_id,
+          'user_id': str(self.user_id),
           'time_stamp': time_now
         }
+      elif kind == "message":
+        
+        message = {
+          'message_id' : str(uuid.uuid4()),
+          'user': self.user,
+          'user_id': str(self.user_id),
+          'message': content,#new attribute so it send message
+          'kind': "message", #to identify source on server side
+          'time_stamp': time_now
+        }
+      elif kind == "users_list":
+        message = {
+          'message_id' : str(uuid.uuid4()),
+          'user': self.user,
+          'user_id': str(self.user_id),
+          'kind': "users_list", #to identify source on server side
+          'time_stamp': time_now
+        }
+      elif kind == "users_messages":
+        message = {
+          'message_id' : str(uuid.uuid4()),
+          'user': self.user,
+          'user_id': self.user_id,
+          'kind': "users_messages", #to identify source on server side
+          'time_stamp': time_now
+        }
+      else:
+        print("no!")
       
-      message_body = json.dumps(message)
+      body = json.dumps(message)
       
       self.channel.basic_publish(
         exchange='',
-        routing_keys='pending_messages',
-        body=message_body,
+        routing_key='pending_messages',
+        body=body,
         properties=pika.BasicProperties(content_type="text/plain", delivery_mode=2) #persistent message
       )
 
+  def Connect(self):
+    self.checked = threading.Event()
+    while not self.is_on:
+      user = input("Ingrese usuario: ")
+      if user.strip():
+        self.send_message(user, "log_in")
+        self.checked.wait()
+        self.checked.clear()
+
+    self.user = user
+
+  def Leave(self):
+    self.send_message("log_out", "log_out")
+    self.connection.close()
   
 if __name__ == '__main__':
   logging.basicConfig()
@@ -80,18 +189,23 @@ if __name__ == '__main__':
 
   client.Connect()
 
+  threading.Thread(target=client.get_server_msgs(), daemon=True).start()
+
+
+  threading.Thread(target=client.get_msgs(), daemon=True).start()
+
   while True:
     user_input = input()
     
     if user_input == "list_users!":
-        client.send_message(user_input, "users_list")
+      client.send_message(user_input, "users_list")
 
     elif user_input == "list_messages!":
-        client.send_message(user_input, "user_messages")
+      client.send_message(user_input, "users_messages")
 
     elif user_input == "leave!":
-        client.Leave()
-        break
+      client.Leave()
+      break
 
     # Envío de un mensaje normal.
     else:
--- Actividad2/server/server.py
+++ Actividad2/server/server.py
@@ -2,12 +2,15 @@ import pika
 import json
 from datetime import datetime
 import uuid
+import time
 
 
 chats = {}
-users_on = {}
+users_on = []
 
-connection = pika.BlockingConnection(pika.ConnectionParameteres('rabbitmq'))
+time.sleep(13) #pika throws error if not used
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
 channel = connection.channel()
 
 
@@ -19,34 +22,94 @@ channel.exchange_declare(exchange="user_channel", exchange_type="direct") #for w
 def on_request(ch, method, props, body):
 
   user_message = body.decode("utf-8")#for the standards
-  json_message = json.looads(user_message)
+  json_message = json.loads(user_message)
 
   time_now = datetime.timestamp(datetime.now())
 
   user_id = json_message["user_id"]
-  content = json_message["content"]
+  user = json_message["user"]
   kind = json_message["kind"] #to filter
 
   if kind == "log_in":
-    done = False
-    if content not in users_on:
-      done = True
-      users_on.append(content)
+    done = "Nope"
+    if user not in users_on:
+      done = "Yay"
+      users_on.append(user)
     
     #now then we send the confirmation message
     response = {
       'message_id' : str(uuid.uuid4()),
-        'kind': "log_in",
-        'user': content,
-        'user_id': user_id,
-        'time_stamp': time_now
+      'kind': "log_in",
+      'response': done,
+      'user_id': user_id,
+      'time_now': time_now
     }
-
     body = json.dumps(response)
+    channel.basic_publish(
+      exchange='user_channel', 
+      routing_key=user_id, 
+      body=body
+    )
+  elif kind == "log_out":
+    user = json_message["user"]
+    users_on.remove(user)
+  elif kind == "message":
+    if user not in chats:
+      chats[user] = [user_message]
+    else:
+      chats[user].append(user_message)
+
+    body = json.dumps(json_message)
+    channel.basic_publish(
+      exchange='broadcast', #since all users will see this message 
+      routing_key='', 
+      body=body
+    )
+    time_sender = json_message["time_now"]
+    json_message["time_now"] = time_now
+    file = open("log.txt","a")
+    server_date = (datetime.fromtimestamp(time_now)).strftime("%d/%m/%Y - %H:%M:%S")
+    user_time = (datetime.fromtimestamp(time_sender)).strftime("%d/%m/%Y - %H:%M:%S")
+    file.write("[" + json_message["message_id"] + "] - " )
+    file.write("user: " + user + ", user_id: " + user_id + ", ")
+    file.write("server_date: " + server_date + ", user_date: " + user_time + ", ")
+    file.write("message: " + json_message["message"] + "\n")
+    file.close()
+
+    file.write()
+  elif kind == "users_list":
+    response_message = {
+        'message_id': str(uuid.uuid4()),
+        'type': "users_list",
+        'response': users_on
+    }
+    body_response = json.dumps(response_message)
+    channel.basic_publish(
+      exchange='user_channel', 
+      routing_key=user_id, 
+      body=body
+    )
+  elif kind == "users_messages":
+    if user in chats:
+        user_messages = chats[user]
+    else:
+      user_messages = []
+      response_message = {
+        'message_id': str(uuid.uuid4()),
+        'kind': "user_messages",
+        'response': user_messages
+      }
+
+    body_response = json.dumps(response_message)
 
     channel.basic_publish(
       exchange='user_channel', 
       routing_key=user_id, 
       body=body
     )
+  channel.basic_ack(delivery_tag=method.delivery_tag)
 
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(queue='pending_messages', on_message_callback=on_request)
+
+channel.start_consuming()
